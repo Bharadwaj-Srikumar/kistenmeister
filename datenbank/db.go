970791d62ec81a12326ff16700347d7f48c2c8b6
Bharad Srikumar <bharadwajsri5@gmail.com>
2024-05-03 18:07:18 +0200

    initial commit

---
+++ datenbank/db.go
@@ -0,0 +1,118 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	// "time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type DB struct {
+	Database *sql.DB
+}
+
+/*type kisten struct {
+	id               int
+	Name             string
+	Beschreibung     string
+	Ersteller        string
+	Erstellungsdatum string
+	Änderer          string
+	Änderungsdatum   string
+	Verantwortlicher string
+	QRCode           string
+	Ort              string
+}*/
+
+func (d *DB) InitDB() {
+	db, err := sql.Open("sqlite3", "/data/database/photo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	d.Database = db
+
+	initStmt := `
+	CREATE TABLE IF NOT EXISTS Kiste (id INTEGER PRIMARY KEY, Name TEXT, Beschreibung TEXT, Ersteller TEXT, Erstellungsdatum DATETIME, Änderer TEXT, Änderungssdatum DATETIME, Verantwortlicher TEXT, QRCode BLOB, Ort TEXT);
+	CREATE TABLE IF NOT EXISTS Kommentare(id INTEGER primary key not null, Kommentar text, Ersteller text,  Erstellungsdatum datetime, FOREIGN KEY(box_id) REFERENCES kisten(id));
+	CREATE TABLE bilder(id INTEGER primary key not null, bild BLOB,Ersteller text, Erstellungsdatum datetime,FOREIGN KEY(box_id) REFERENCES kisten(id)),`
+
+	_, err = d.Database.Exec(initStmt)
+	if err != nil {
+		log.Fatalf("%q: %s\n", err, initStmt)
+	}
+}
+
+/*
+func (d *DB) AddPhoto(name string, timeTaken int) error {
+	tx, err := d.Database.Begin()
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.Prepare("insert into PHOTOS(FILENAME, LAST_VIEWED, TIME_TAKEN) values(?,?,?)")
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(name, 0, timeTaken)
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
+
+	return err
+}
+
+func (d *DB) UpdateLastViewed(name string) error {
+	tx, err := d.Database.Begin()
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.Prepare("UPDATE PHOTOS SET LAST_VIEWED=? WHERE FILENAME=?")
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(time.Now().Unix(), name)
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
+
+	return err
+}
+
+func (d *DB) GetNextSlideshowPhoto() (PhotoDetails, error) {
+	rows, err := d.Database.Query("select FILENAME, LAST_VIEWED, TIME_TAKEN from PHOTOS ORDER BY LAST_VIEWED LIMIT 1")
+	if err != nil {
+		return PhotoDetails{}, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		details := PhotoDetails{}
+		err := rows.Scan(&details.Name, &details.LastViewed, &details.TimeTaken)
+		if err != nil {
+			return PhotoDetails{}, nil
+		}
+		return details, nil
+	}
+
+	err = rows.Err()
+	return PhotoDetails{}, err
+}
+
+func (d *DB) GetPhotoList(count int, offset int) ([]PhotoDetails, error) {
+
+	return []PhotoDetails{}, nil
+}
+*/
---
+++ main_bs.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const dbPath = "./kistenmeister/Kistenmeister.db"
+
+/*type DB struct {
+	Database *sql.DB
+}*/
+
+/*type Routes struct {
+	Database db.DB
+}*/
+
+func setupRouter() *gin.Engine {
+
+	r := gin.Default()
+
+	// Route zum Erstellen der Datenbank, wenn nicht bereits vorhanden
+	r.GET("/createDB", func(c *gin.Context) {
+		// Prüfen, ob die Datenbank bereits existiert
+		if _, err := os.Stat(dbPath); err == nil { // wenn Datenbank schon vorhanden
+			c.String(http.StatusOK, "Datenbank existiert bereits")
+		} else if db, err := sql.Open("sqlite3", dbPath); err == nil { // wenn Datenbank nicht vorhanden
+			defer db.Close()
+			c.String(http.StatusCreated, "Datenbank erfolgreich erstellt")
+		} else { // Fehler bei der Eingabe des Pfads
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Fehler bei der Datenbankerstellung: %v", err))
+			return
+		}
+	})
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	// Read box
+	//TODO prüfen ob HEAD oder GET besser passt
+	r.GET("/box/:id", func(c *gin.Context) {
+		id := c.Params.ByName("id")
+		//TODO Get box data from SQlite
+
+		//TODO Return box data
+		c.JSON(http.StatusOK, gin.H{"box read": id})
+	})
+
+	// Create box
+	r.POST("/box", func(c *gin.Context) {
+		//TODO Create box data in SQlite
+
+		//TODO Return box data
+		c.JSON(http.StatusOK, gin.H{"box create": "newid"})
+	})
+
+	// Update box
+	//TODO prüfen ob PATCH oder PUT besser passt
+	r.PUT("/box/:id", func(c *gin.Context) {
+		id := c.Params.ByName("id")
+
+		//TODO Update box data in SQlite
+
+		//TODO Return box data
+		c.JSON(http.StatusOK, gin.H{"box update": id})
+	})
+
+	// Delete box
+	r.DELETE("/box/:id", func(c *gin.Context) {
+		id := c.Params.ByName("id")
+
+		//TODO Delete box data in SQlite
+
+		//TODO Return box data
+		c.JSON(http.StatusOK, gin.H{"box delete": id})
+	})
+
+	// Get all boxes
+	r.GET("/boxes", func(c *gin.Context) {
+		//TODO Get all boxes data from SQlite
+
+		//TODO Return all boxes data
+		c.JSON(http.StatusOK, gin.H{"boxes": "all"})
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	// Listen and Serve in 0.0.0.0:8080
+	r.Run(":8080")
+}
