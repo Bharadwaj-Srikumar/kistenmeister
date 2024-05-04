1bc5dc45253a605c035efd9a67899fba9641774d
Bharadwaj-Srikumar <bharadwajsri5@gmail.com>
2024-05-04 23:26:08 +0200

    getKisten push

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.22.2
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/daviddengcn/go-colortext v1.0.0 // indirect
+	github.com/dixonwille/wmenu v4.0.2+incompatible // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.9.1 // indirect
@@ -28,5 +30,6 @@ require (
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/dixonwille/wlog.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,109 +2,145 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	//"fmt"
 	"log"
 	"net/http"
-	"os"
+
+	//"os"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const dbPath = "./kistenmeister/datenbank/db"
+var DB *sql.DB
 
-type DB struct {
-	Database *sql.DB
+func ConnectDatabase() error {
+	db, err := sql.Open("sqlite3", "./kistenmeister.db")
+	if err != nil {
+		return err
+	}
+
+	DB = db
+	return nil
 }
 
-func setupRouter() *gin.Engine {
+type kiste struct {
+	id               int    `json:"id"`
+	Name             string `json:"Name"`
+	Beschreibung     string `json:"Beschreibung"`
+	Ersteller        string `json:"Ersteller"`
+	Erstellungsdatum string `json:"Erstellungsdatum"`
+	Änderer          string `json:"Änderer"`
+	Änderungssdatum  string `json:"Änderungssdatum"`
+	Verantwortlicher string `json:"Verantwortlicher"`
+	//QRCode           string `json:"QRCode"`
+	Ort string `json:"Ort"`
+}
 
-	r := gin.Default()
-	var d *DB
-
-	// Route zum Erstellen der Datenbank, wenn nicht bereits vorhanden
-	r.GET("/createDB", func(c *gin.Context) {
-		// Prüfen, ob die Datenbank bereits existiert
-		if _, err := os.Stat(dbPath); err == nil { // wenn Datenbank schon vorhanden
-			c.String(http.StatusOK, "Datenbank existiert bereits")
-		} else if db, err := sql.Open("sqlite3", dbPath); err == nil { // wenn Datenbank nicht vorhanden
-			// TODO Datenbank
-			d.Database = db
-
-			initStmt := `
-				CREATE TABLE IF NOT EXISTS Kiste (id INTEGER PRIMARY KEY, Name TEXT, Beschreibung TEXT, Ersteller TEXT, Erstellungsdatum DATETIME, Änderer TEXT, Änderungssdatum DATETIME, Verantwortlicher TEXT, QRCode BLOB, Ort TEXT);
-				CREATE TABLE IF NOT EXISTS Kommentare(id INTEGER primary key not null, Kommentar text, Ersteller text,  Erstellungsdatum datetime, FOREIGN KEY(box_id) REFERENCES kisten(id));
-				CREATE TABLE bilder(id INTEGER primary key not null, bild BLOB,Ersteller text, Erstellungsdatum datetime,FOREIGN KEY(box_id) REFERENCES kisten(id)),`
-
-			_, err = d.Database.Exec(initStmt)
-			if err != nil {
-				log.Fatalf("%q: %s\n", err, initStmt)
-			}
-			defer db.Close()
-			c.String(http.StatusCreated, "Datenbank erfolgreich erstellt")
-		} else { // Fehler bei der Eingabe des Pfads
-			c.String(http.StatusInternalServerError, fmt.Sprintf("Fehler bei der Datenbankerstellung: %v", err))
-			return
-		}
-	})
-
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	// Read box
-	//TODO prüfen ob HEAD oder GET besser passt
-	r.GET("/box/:id", func(c *gin.Context) {
-		id := c.Params.ByName("id")
-		//TODO Get box data from SQlite
-
-		//TODO Return box data
-		c.JSON(http.StatusOK, gin.H{"box read": id})
-	})
-
-	// Create box
-	r.POST("/box", func(c *gin.Context) {
-		//TODO Create box data in SQlite
-
-		//TODO Return box data
-		c.JSON(http.StatusOK, gin.H{"box create": "newid"})
-	})
-
-	// Update box
-	//TODO prüfen ob PATCH oder PUT besser passt
-	r.PUT("/box/:id", func(c *gin.Context) {
-		id := c.Params.ByName("id")
-
-		//TODO Update box data in SQlite
-
-		//TODO Return box data
-		c.JSON(http.StatusOK, gin.H{"box update": id})
-	})
-
-	// Delete box
-	r.DELETE("/box/:id", func(c *gin.Context) {
-		id := c.Params.ByName("id")
-
-		//TODO Delete box data in SQlite
-
-		//TODO Return box data
-		c.JSON(http.StatusOK, gin.H{"box delete": id})
-	})
-
-	// Get all boxes
-	r.GET("/boxes", func(c *gin.Context) {
-		//TODO Get all boxes data from SQlite
-
-		//TODO Return all boxes data
-		c.JSON(http.StatusOK, gin.H{"boxes": "all"})
-	})
-
-	return r
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-	r := setupRouter()
+	ConnectDatabase()
+	r := gin.Default()
+	// API v1
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/kisten", getKisten)
+		v1.GET("/kiste/:id", getKiste)
+		v1.POST("/kiste", createKiste)
+		v1.PUT("/kiste/:id", aktKiste)
+		v1.DELETE("/kiste/:id", löschenKiste)
+	}
 	// Listen and Serve in 0.0.0.0:8080
 	r.Run(":8080")
+}
+
+// Read box
+// TODO prüfen ob HEAD oder GET besser passt
+func getKiste(c *gin.Context) {
+	ID := c.Params.ByName("id")
+	//TODO Get box data from SQlite
+
+	//TODO Return box data
+	c.JSON(http.StatusOK, gin.H{"Kiste lesen": ID})
+}
+
+// Create box
+func createKiste(c *gin.Context) {
+	//TODO Create box data in SQlite
+
+	//TODO Return box data
+	c.JSON(http.StatusOK, gin.H{"Kiste erstellen": "newid"})
+}
+
+// Update box
+// TODO prüfen ob PATCH oder PUT besser passt
+func aktKiste(c *gin.Context) {
+	ID := c.Params.ByName("id")
+
+	//TODO Update box data in SQlite
+
+	//TODO Return box data
+	c.JSON(http.StatusOK, gin.H{"Kiste aktualiseren": ID})
+}
+
+// Delete box
+func löschenKiste(c *gin.Context) {
+	ID := c.Params.ByName("id")
+
+	//TODO Delete box data in SQlite
+
+	//TODO Return box data
+	c.JSON(http.StatusOK, gin.H{"Kiste löschen": ID})
+}
+
+func getKisten(c *gin.Context) {
+
+	alleKisten, err := GetKisten()
+	checkErr(err)
+
+	if alleKisten == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No Records Found"})
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{"data": alleKisten})
+	}
+
+}
+
+// Get all boxes
+func GetKisten() ([]kiste, error) {
+
+	rows, err := DB.Query("SELECT id, Name, Beschreibung, Ersteller, Erstellungsdatum, Änderer, Änderungssdatum, Verantwortlicher, Ort from Kiste;")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	alleKisten := make([]kiste, 0)
+
+	for rows.Next() {
+		einzelneKiste := kiste{}
+		err = rows.Scan(&einzelneKiste.id, &einzelneKiste.Name, &einzelneKiste.Beschreibung, &einzelneKiste.Ersteller, &einzelneKiste.Erstellungsdatum, &einzelneKiste.Änderer, &einzelneKiste.Änderungssdatum, &einzelneKiste.Verantwortlicher, &einzelneKiste.Ort)
+
+		if err != nil {
+			return nil, err
+		}
+
+		alleKisten = append(alleKisten, einzelneKiste)
+	}
+
+	err = rows.Err()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return alleKisten, err
 }
